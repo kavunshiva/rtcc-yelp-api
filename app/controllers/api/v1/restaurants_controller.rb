8b0de36cde943ebd9fc33853eb71883bb6dd2064
kavunshiva <kavunshiva@gmail.com>
2017-08-14 19:16:56 -0400

    clean up controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,20 +1,29 @@
 class Api::V1::RestaurantsController < ApplicationController
   def get_restaurants_from_yelp_by_city
-    uri = URI.parse("https://api.yelp.com/v3/businesses/search?location=#{params[:city]}")
+    uri = URI.parse(uri_string)
     request = Net::HTTP::Get.new(uri)
     request["Authorization"] = "Bearer #{ENV['token']}"
 
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    response = Net::HTTP.start(uri.hostname, uri.port, {use_ssl: uri.scheme == "https"}) do |http|
       http.request(request)
     end
 
     businesses = response.body
 
     render json: businesses
+  end
+
+  private
+
+  def uri_string
+    uri = "https://api.yelp.com/v3/businesses/search?limit=20&location=#{params[:city]}"
+    if params[:sort_by] == "rating"
+      uri += '&sort_by=rating'
+    end
+    if params[:price]
+      uri += "&price=#{params[:price]}"
+    end
+    return uri
   end
 
 end
